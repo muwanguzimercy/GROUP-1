3efc78c7d2ccbb088d2cf3068f6d53b4e02ce1a8
NabahindaAisha <nabahindaasha@gmail.com>
2025-09-02 18:29:09 +0300

    Update ServiceSeeder.php

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -1,1 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Service;
+
+class ServiceSeeder extends Seeder
+{
+    public function run()
+    {
+        Service::create([
+            'facility_id' => 1,
+            'name' => 'PCB Fabrication',
+            'description' => 'Design and manufacture of printed circuit boards.',
+            'category' => 'Machining',
+            'skill_type' => 'Hardware',
+        ]);
+    }
+}
+
 
