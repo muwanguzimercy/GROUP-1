11b9098c3d44958af88b1bcacae2d53d163f57bf
nakiguli-elizabeth-gloria <nakiguligloria5@gmail.com>
2025-09-02 18:04:51 +0300

    Update Participant.php

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
-    //
+    protected $fillable = [
+        'full_name',
+        'email',
+        'affiliation',
+        'specialization',
+        'cross_skill_trained',
+        'institution',
+    ];
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class, 'project_participant')
+                    ->withPivot('role_on_project', 'skill_role')
+                    ->withTimestamps();
+    }
 }
