9f622b4219daf4508b2fbf56abf895223f3f7978
NabahindaAisha <nabahindaasha@gmail.com>
2025-09-02 18:17:53 +0300

    Update Service.php

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $fillable = [
+        'facility_id',
+        'name',
+        'description',
+        'category',
+        'skill_type',
+    ];
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
 }
