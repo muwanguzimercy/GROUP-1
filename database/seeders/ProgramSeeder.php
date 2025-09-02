57183697f1b373c9cc8143e56874ed3eb6723156
Apoul James <123954682+Apouljames@users.noreply.github.com>
2025-09-03 00:10:21 +0300

    Update ProgramSeeder.php

--- database/seeders/ProgramSeeder.php
+++ database/seeders/ProgramSeeder.php
@@ -1,1 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Program;
+
+class ProgramSeeder extends Seeder
+{
+    public function run()
+    {
+        Program::create([
+            'name' => 'Smart Agriculture Initiative',
+            'description' => 'Enhancing crop yield prediction using IoT and AI.',
+            'national_alignment' => 'NDPIII - Agro-industrialization',
+            'focus_areas' => 'IoT, Machine Learning, Sustainability',
+            'phases' => 'Cross-Skilling, Collaboration, Prototyping, Commercialization',
+        ]);
+    }
+}
 
