cf6f40cc330be7558990da2849537d74cd90be8f
Apoul James <123954682+Apouljames@users.noreply.github.com>
2025-09-03 01:14:29 +0300

    Update Facilities.php

--- app/Models/Facilities.php
+++ app/Models/Facilities.php
@@ -4,7 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Facilities extends Model
+class Facility extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'location',
+        'description',
+        'partner_organization',
+        'facility_type',
+        'capabilities',
+    ];
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
+
+    public function equipment()
+    {
+        return $this->hasMany(Equipment::class);
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
