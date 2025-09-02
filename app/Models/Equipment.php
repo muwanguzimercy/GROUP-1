2a50c8959ab5c3f7b9e3b3372ed3cd408b4f05e5
muwanguzimercy <mercymuwanguzi23@gmail.com>
2025-09-02 18:06:49 +0300

    Update Equipment.php

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipment extends Model
 {
-    //
+    protected $fillable = [
+        'facility_id',
+        'name',
+        'capabilities',
+        'description',
+        'inventory_code',
+        'usage_domain',
+        'support_phase',
+    ];
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
 }
