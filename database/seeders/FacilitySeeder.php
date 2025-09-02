dea262b915a7894d25a9c7b1aeddd92b24603e97
Apoul James <123954682+Apouljames@users.noreply.github.com>
2025-09-03 02:03:33 +0300

    Update FacilitySeeder.php

--- database/seeders/FacilitySeeder.php
+++ database/seeders/FacilitySeeder.php
@@ -1,1 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Facility;
+
+class FacilitySeeder extends Seeder
+{
+    public function run()
+    {
+        Facility::create([
+            'name' => 'Lwera Innovation Lab',
+            'location' => 'Lwera, Uganda',
+            'description' => 'Government-backed lab for prototyping and testing.',
+            'partner_organization' => 'UIRI',
+            'facility_type' => 'Testing Center',
+            'capabilities' => 'CNC, PCB fabrication, materials testing',
+        ]);
+    }
+}
 
